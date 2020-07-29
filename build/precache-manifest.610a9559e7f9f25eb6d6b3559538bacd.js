a1138563b173bffd1e865cf0a1c8ccf48d531d7a
Amir Mussa <amir.mussa99@gmail.com>
2020-07-29 16:19:12 -0500

    build added

---
+++ build/precache-manifest.610a9559e7f9f25eb6d6b3559538bacd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a968597f4b8f66ab78a91173298b91ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7809187ad18b949db72",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "6c1cbdccc6a517fe0f05",
+    "url": "/static/js/2.79b471ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.79b471ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7809187ad18b949db72",
+    "url": "/static/js/main.1a041139.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
